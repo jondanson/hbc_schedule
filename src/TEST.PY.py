efadc0bfb2dcff7302af206ce27954e5cab429b8
jondanson <jondanson@gmail.com>
2016-09-03 22:39:06 -0700

    Just a patch

--- src/TEST.PY.py
+++ src/TEST.PY.py
@@ -7,24 +7,25 @@ from src.common.database import Database
 
 Database.initialize()
 
-#mem=[]
+# mem = Member.all()
 
-#mem = Member.all_members()
+# print (type (mem))
 
-#print (type (mem))
-
-#for members in mem:
+# for members in mem:
 #    print (members._id)
 
-#event = Event("Sunday Morning Service", "2016-7-15", "Security",
-              #[mem[0]._id, mem[1]._id, mem[2]._id, mem[3]._id, mem[4]._id])
+# event = Event("Sunday Evening Service", "2016-9-15", "Security", [mem[0]._id, mem[1]._id, mem[2]._id, mem[3]._id, mem[4]._id])
 
 
 
-#event.save_to_mongo()
+# event.save_to_mongo()
 
-events = Event.find_notifications(7)
-
+events = Event.all()
+event1 = []
 for event in events:
-    event.week_before_reminder()
-    print(event.tag, event.title)
+    this = Event.find_by_id(event._id)
+
+    for it in this:
+        print(it.title)
+
+
---
+++ src/static/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jonda_000 on 8/31/2016.
+ */
+$('.mobileSelect').mobileSelect({
+    animation: 'scale',
+    animationSpeed: 400
+});
