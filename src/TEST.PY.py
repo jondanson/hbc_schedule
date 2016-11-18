a3dfc8945845426ce0964488e5931bf93c421105
jondanson <jondanson@gmail.com>
2016-11-17 20:03:41 -0800

    Fixed a few bugs.

--- src/TEST.PY.py
+++ src/TEST.PY.py
@@ -1,37 +1,15 @@
-import time
-import datetime
-from datetime import date, timedelta
+
 from src.models.members.members import Member
-from src.models.admin.admin import Admin
 from src.models.events.events import Event
 from src.common.database import Database
 
 Database.initialize()
 
-# mem = Member.all()
-
-# print (type (mem))
-
-# for members in mem:
-#    print (members._id)
-
-# event = Event("Sunday Evening Service", "2016-9-15", "Security", [mem[0]._id, mem[1]._id, mem[2]._id, mem[3]._id, mem[4]._id])
-
-
-
-# event.save_to_mongo()
-
-events = Event.all()
-event1 = []
+events = Event.find_notifications(6)
 
 for event in events:
-    this = Event.find_by_id(event._id)
-
-    for it in this:
-        print(it.title)
-
-
-# user = "jondanson@gmail.com"
-# pword = "test"
-#
-# Admin.register_admin(user, pword)
+    print(event.ministry)
+    for member in event.assigned_members:
+        member = Member.find_by_id(member)
+        event.event_email(member.email, member.first_name, member.last_name)
+        print(member.first_name + " " + member.last_name + " " + member.email)
